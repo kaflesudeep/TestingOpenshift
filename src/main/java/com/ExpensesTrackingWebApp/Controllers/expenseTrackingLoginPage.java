888ebc1e7c0c4b07b9f81a71749071d8fde091ac
kaflesudeep <kaflesudeepjava@gmail.com>
2018-01-11 21:14:52 -0500

    xyz

---
+++ src/main/java/com/ExpensesTrackingWebApp/Controllers/expenseTrackingLoginPage.java
@@ -0,0 +1,27 @@
+package com.ExpensesTrackingWebApp.Controllers;
+
+	import javax.servlet.http.HttpServlet;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.servlet.ModelAndView;
+
+	@Controller
+	public class expenseTrackingLoginPage extends HttpServlet {
+		@RequestMapping("/CouponTraking-0.0.1-SNAPSHOT/welcome")
+		public ModelAndView precouponTrackingLoginPage(){
+			ModelAndView modelAndView = new ModelAndView("expenseTrackingLoginPage"); 
+			System.out.println("1dsd");
+			return modelAndView; 
+					
+		}
+		
+		@RequestMapping("/CouponTraking-0.0.1-SNAPSHOT/loginUser")
+		public ModelAndView postexpenseTrackingLoginPage(){
+			System.out.println("dsd");
+			ModelAndView modelAndView = new ModelAndView("expenseTrackingHomePage"); 
+			
+			return modelAndView; 
+					
+		}
+		
+	}
